5c3f3df2c1f62066b9aaffb885865b85f4b0bea7
Jan Kaspar <jan.kaspar@cern.ch>
2017-07-27 14:37:08 +0200

    Updated after modifications of CTPPSFastProtonSimulation module.

--- SimCTPPS/OpticsParameterisation/python/ctppsFastProtonSimulation_cfi.py
+++ SimCTPPS/OpticsParameterisation/python/ctppsFastProtonSimulation_cfi.py
@@ -4,32 +4,28 @@ from SimCTPPS.OpticsParameterisation.ctppsDetectorPackages_cff import detectorPa
 from SimCTPPS.OpticsParameterisation.lhcBeamConditions_cff import lhcBeamConditions_2016PreTS2
 
 ctppsFastProtonSimulation = cms.EDProducer('CTPPSFastProtonSimulation',
-    beamParticlesTag = cms.InputTag('generator', 'unsmeared'),
+    hepMCTag = cms.InputTag('generator', 'unsmeared'),
+
     beamConditions = lhcBeamConditions_2016PreTS2,
 
-    sqrtS = cms.double(13.0e3),
-
+    # TODO: remove
     # crossing angle
-    halfCrossingAngleSector45 = cms.double(179.394e-6), # in rad
-    halfCrossingAngleSector56 = cms.double(191.541e-6), # in rad
-
-    detectorPackages = detectorPackages_2016PreTS2,
-    simulateDetectorsResolution = cms.bool(True),
-    produceHitsRelativeToBeam = cms.bool(False),
-
-    roundToPitch = cms.bool(False),
-    pitch = cms.double(66.e-3), # mm
-    insensitiveMargin = cms.double(34.e-3), # mm
-
-    checkApertures = cms.bool(True),
+    #halfCrossingAngleSector45 = cms.double(179.394e-6), # in rad
+    #halfCrossingAngleSector56 = cms.double(191.541e-6), # in rad
 
     opticsFileBeam1 = cms.FileInPath('CondFormats/CTPPSOpticsObjects/data/2016_preTS2/version4-vale1/beam1/parametrization_6500GeV_0p4_185_reco.root'),
     opticsFileBeam2 = cms.FileInPath('CondFormats/CTPPSOpticsObjects/data/2016_preTS2/version4-vale1/beam2/parametrization_6500GeV_0p4_185_reco.root'),
 
-    scatteringAngle = cms.double(25.e-6), # in rad
-    simulateVertexX = cms.bool(True),
-    simulateVertexY = cms.bool(True),
-    simulateScatteringAngleX = cms.bool(True),
-    simulateScatteringAngleY = cms.bool(True),
-    simulateBeamDivergence = cms.bool(True),
+    detectorPackages = detectorPackages_2016PreTS2,
+
+    produceScoringPlaneHits = cms.bool(True),
+    produceRecHits = cms.bool(True),
+
+    produceHitsRelativeToBeam = cms.bool(False),
+
+    checkApertures = cms.bool(True),
+
+    roundToPitch = cms.bool(False),
+    pitch = cms.double(66.e-3), # mm
+    insensitiveMargin = cms.double(34.e-3), # mm
 )
